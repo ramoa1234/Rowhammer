e738b6092885c4c8de69575151738c47d1b5ec49
ramoa1234 <antonior12345@icloud.com>
2025-06-30 04:04:56 -0400

    test

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
-
-void hammer() {
-
-
-    
+void hammer(char x, char y, char w) {
+    //repeadtly read and write into x and w(physical memory avoiding cache) 
+    char new_x = '1';
+    memcpy(&x, &new_x, sizeof(char));
+    printf("New value of x = %c", new_x);
 }
 
+
+void row_hammer(x, y, w) { 
+
+    for(int i = 0;i < 1000;i++) {
+        hammer(x, y, w);
+
+    }
+
+}
 
 
 int main() {
@@ -20,6 +30,7 @@ int main() {
     
     //goal is to rewrite or read the value at y
 
+    hammer(x, y, w);
 
     return 0;
 }
